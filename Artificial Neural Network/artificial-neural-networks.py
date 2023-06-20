08df8f72a56507cc8282b1548a67a9128ca22b49
ricky-kiva <ricky.kiva@gmail.com>
2023-06-20 20:27:32 +0700

    add Backpropagation summary

--- Artificial Neural Network/artificial-neural-networks.py
+++ Artificial Neural Network/artificial-neural-networks.py
@@ -30,3 +30,45 @@
 # caution on learning rate:
 # - large learning rate can result missing the minumum
 # - small learning rate can result long time to find minimum point
+
+### BACKPROPAGATION
+
+# Backpropagation: is a way neural networks train & optimize their weights & biases for gien problem & data set
+# - it happened after the network have already making predictions using forward propagation, which gave the initial weights & bias
+
+# Supervised Learning: each data point has corresponding 'Label' / 'Ground Truth'
+# - trainig is needed when predicted value of neural network doesn't match Ground Truth
+
+# ways of Backpropagation:
+# - calculate error (E) between Ground Truth & estimated output
+# --- the error represents the cost function
+# - propagate the error back into the network & update each weight & bias using these equations:
+# --- wi -> wi - (learning rate) • dE/dwi
+# --- bi -> bi - (learning rate) • dE/dbi
+
+# given this simple neural network: 2-2-backpropagation-algorithm.JPG
+# - E = (1/2m) Σ (T - a[2,i])^2
+# --- T = Ground Truth
+
+# from the given simple neural network, we need to update each element as follows:
+# - w2 -> w2 - (learning rate) • dE/dw2
+# - b2 -> b2 - (learning rate) • dE/db2
+# - w1 -> w1 - (learning rate) • dE/dw1
+# - b1 -> b1 - (learning rate) • dE/db1
+# --- full calculations could be found in: 2-2-partial derivative-example.pdf
+
+# given calculated forward propagation for the simple neural network for input 0.1:
+# - 2-2-backpropagation-calculated-forward-prop.JPG
+
+# assume the ground truth is 0.25
+# - then we plug the calculated values (a2, a1, z2, z1) to update
+# - update values from: w2 -> b2 -> w1 -> b1
+# - the formula to calculate is the one within 2-2-partial derivative-example.pdf
+
+# summarize of way: Complete Training Algorithm
+# - initializy `weights` & `biases` randomly
+# - iteratively repeat following steps:
+# --- calculate output using forward propagation
+# --- calculate error between Ground Truth & predicted output
+# --- update weights & biases through Backpropagation
+# --- repeat above 3 steps until number of epoch is reached / output is elow predefined threshold
