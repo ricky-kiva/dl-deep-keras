442e1d8644bc35b5a2016b0be1792889e4d408e4
ricky-kiva <ricky.kiva@gmail.com>
2023-06-20 23:28:21 +0700

    add Deep Learning Libraries summary

---
+++ Deep Learning Libraries/deep-learning-libraries.py
@@ -0,0 +1,18 @@
+### DEEP LEARNING LIBRARIES
+
+# Popular Deep Learnin libraries:
+# - TensorFlow
+# - Pytorch
+# - Keras
+# - Theano (ou-of-development)
+
+# TensorFlow released to public in 2015 by Google
+# - it's mostly used in production of deep learning models
+
+# Pytorch derived from Torch framework in Lua, released publicly in 2016
+# - eventhough it's derived, it's still tailored to be fast & native
+# mostly used in academic settings & deep learning applications requiring optimizing custom expressions
+
+# Keras is a high level API to build deep learning models
+# - it's gained favor for its ease of use & syntatic simplicity for fast development
+# - it runs on top of a low-lvel library like TensorFlow
