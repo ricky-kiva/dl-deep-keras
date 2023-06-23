6c804bb35d28cf496f3934e7e7dd9ba68de88311
ricky-kiva <ricky.kiva@gmail.com>
2023-06-23 23:17:26 +0700

    add Deep neural network summary

---
+++ Deep Learning Models/deep-learning-models.py
@@ -0,0 +1,9 @@
+### SHALLOW VS. DEEP NEURAL NETWORK
+
+# Shallow Neural Network has small number of hidden layers (such as only 1)
+# Deep Neural Network has large number of neurons & hidden layers
+
+# why Deep Learning Took off?
+# - advancement in the field itself (such ReLU)
+# - data (widely available)
+# - computational power (like nVidia GPUs, offers training of large data in the matter of hours/day)
